92fcaf1fa06a92b5ca0f7df3d8d246c87d34e5d8
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-09-20 14:58:32 +0300

    Initial commit

---
+++ contacts.js
@@ -0,0 +1,39 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from 'nanoid'
+
+const contactsPath = path.join('db', 'contacts.json');
+
+export const listContacts = async () => {
+    const date = await fs.readFile(contactsPath);
+    return JSON.parse(date);
+};
+
+export const getContactById = async (contactId) => {
+    const contacts = await listContacts();
+    const contactById = contacts.find(({ id }) =>
+        id === contactId
+    )
+    return contactById || null;
+};
+
+export const removeContact = async (contactId) => {
+    const contacts = await listContacts();
+    const removeIndex = contacts.findIndex(({ id }) =>
+        id === contactId
+    );
+    if (removeIndex === -1) {
+        return null;
+    };
+    const removeContact = contacts.splice(removeIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+    return removeContact;
+};
+
+export const addContact = async (name, email, phone) => {
+    const newContact = { id: nanoid(), name, email, phone };
+    const contacts = await listContacts();
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+};
